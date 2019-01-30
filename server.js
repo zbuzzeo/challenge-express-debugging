c7712004f02090d4b78e91c74c3cccb78494122e
Zekiel Buzzeo <zeke.buzzeo@gmail.com>
2019-01-30 09:29:23 -1000

    debugged all errors on server.js

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
+// error 1: neither packages were installed
 const express = require('express');
 const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-const home = require('./router/home'); // rename this to router
-const about = require('./router/about'); // rename this to router
-const contact = require('./router/contact'); // rename this to router
 
-app.use(express.static('public'));
+// variables were re-routed. also changed to ./routes instead of ./router.
+const home = require('./routes/home/get/index'); // rename this to router?
+const about = require('./routes/about/get/index'); // rename this to router?
+const contact = require('./routes/contact/get/index'); // rename this to router?
+
+// express.statc('public'); is not a route
+app.use(express.static('./public'));
 app.engine('.hbs', exphbs({extname: '.hbs', defaultLayout: 'main'}));
 app.set('view engine', '.hbs');
 
@@ -15,9 +19,10 @@ app.get('/', (req, res) => {
   res.send('smoke test');
 });
 
-app.use(home);
-app.use(about);
-app.use(contact);
+// app.use() needed a second parameter. app.use() says this: at THIS file path, i want THIS file to handle it.
+app.use('/home', home);
+app.use('/about', about);
+app.use('/contact', contact);
 
 app.listen(PORT, () => {
   console.log(`Server started on port: ${PORT}`);
